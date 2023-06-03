38d1c3450da641eb6d22c72ade0a68d736e881ed
adarsh saxena <saxenaadarsh@gmail.com>
2023-06-03 12:08:20 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const link = document.querySelector(".link");
+const transition = document.querySelector(".transition");
+link.addEventListener("click",(e) =>{
+e.preventDefault();    
+transition.classList.add("slide");
+setTimeout(()=>{
+    window.location = link.href;
+},1800)
+});
+
